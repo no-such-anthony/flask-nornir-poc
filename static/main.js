636246b064bfc03300939c64c5ad18c984f57cf2
no-such-anthony <no.such.anthony@gmail.com>
2021-01-06 04:55:25 +0000

    visual improvements

--- static/main.js
+++ static/main.js
@@ -1,22 +1,54 @@
 $(document).ready(function() {
 
-  //click behaviour for main buttons
-  $('#submit').click(function() {
-      submitRun(socket.id);
-  });
+    //click behaviour for main buttons
+    $('#submit').click(function() {
+        submitRun(socket.id);
+    });
 
-  var socket = io();
-  socket.on('connect', function() {
-      console.log('client connected');
-  });
+    var socket = io();
+    socket.on('connect', function() {
+        console.log('client connected');
+    });
 
-  socket.on('update', function(msg) {
-      $("#updates").append('<li>'+msg+'</li>');
-  });
+    socket.on('update', function(msg) {
+        $("#updates").append('<li>'+msg+'</li>');
+    });
 
-  socket.on('progress', function(msg) {
-    $("#progress").html(msg).show();
-  });
+    socket.on('progress', function(msg) {
+        $("#progress").html(msg).show();
+    });
+
+    // Hook up ACE editor to all textareas with data-editor attribute
+    $(function () {
+        $('textarea[data-editor]').each(function () {
+            var textarea = $(this);
+            var mode = textarea.data('editor');
+
+            var editDiv = $('<div>', {
+                position: 'absolute',
+                width: textarea.width(),
+                height: textarea.height(),
+                'class': textarea.attr('class')
+            }).insertBefore(textarea);
+
+            textarea.css('visibility', 'hidden');
+            textarea.css('width', '0');
+            textarea.css('height', '0');
+
+            var editor = ace.edit(editDiv[0]);
+            editor.renderer.setShowGutter(false);
+            editor.getSession().setValue(textarea.val());
+            editor.getSession().setMode("ace/mode/" + mode);
+            editor.setTheme("ace/theme/twilight");
+            editor.getSession().setTabSize(2);
+
+            // copy back to textarea on form submit...
+            $('#submit').mousedown(function () {
+                textarea.val(editor.getSession().getValue());
+            })
+
+        });
+    });
 
 });
 
